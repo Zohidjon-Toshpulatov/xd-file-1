929e1af8a4cb7e704d6e2495619af856a849afa1
muhammadali1995 <mtoshpulatov3@gmail.com>
2020-07-01 09:06:09 +0500

    commit

--- js/main.js
+++ js/main.js
@@ -1,2 +1,16 @@
 $(document).ready(function(){
+    $("#first-tab-active").on('click', function(){
+        activateTab($("#first-tab"));
+    });
+    $("#second-tab-active").on('click', function(){
+        activateTab($("#second-tab"));
+    });
+    $("#third-tab-active").on('click', function(){
+        activateTab($("#third-tab"));
+    });
+
+   function activateTab(tabId){
+        $(".custom-tab-content").removeClass("active-tab");
+        $(tabId).addClass("active-tab");
+    }
 })
